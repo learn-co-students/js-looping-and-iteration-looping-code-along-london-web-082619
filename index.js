f983dccf7551a9adec87c2f4a4bc75461f3a2526
steve-alex <steve.alex@hotmail.co.uk>
2019-10-06 16:01:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (names, event){
+    let array = []
+    for (let i=0; i < names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array
+}
+
+function countdown(number){
+    while (number >= 0){
+        console.log(number)
+        number -= 1
+    }
+}
