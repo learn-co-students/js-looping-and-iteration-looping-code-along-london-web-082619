61142ddcdf891712f4763323a7ea4b24cb5a44f9
github username <github email address>
2019-10-07 13:40:16 +0100

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 function writeCards(cards, surprise) {
     let greetings = [];
     for (let i = 0; i < cards.length; i++) {
-      greetings.push (`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
-    }
+      greetings.push (`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`)
+    };
   
     return greetings;
-  }
+  };
   
   function countdown(num) {
     while (num >= 0) {
