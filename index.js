11640d00c8221f1e563f449441a0b2ed40118f3b
ck2e14 <chriswkennedy@icloud.com>
2019-10-09 17:57:36 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let tyCards = []
+    for (let i = 0; i < names.length; i++) {
+        tyCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return tyCards
+}
+
+function countdown(countdown) {
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown -= 1;
+    }
+    console.log(countdown)
+}
